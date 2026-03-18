522f44e5642f2b63bc8512ae6661cd78852852ad
Islam035 <e.3rbawy@gmail.com>
2026-03-18 22:22:44 +0200

    تحديث لـ V1.15

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ const LOCAL_DB_FILE = path.join(__dirname, 'local-storage.json');
 let isDbConnected = false;
 
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
